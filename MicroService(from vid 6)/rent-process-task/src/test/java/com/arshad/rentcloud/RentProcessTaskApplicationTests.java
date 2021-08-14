eed025a0fca9c7a489f43e0438279c1918e66530
Arshad Suraj <moharshad46@gmail.com>
2021-08-14 11:58:55 +0530

    spring task added

---
+++ MicroService(from vid 6)/rent-process-task/src/test/java/com/arshad/rentcloud/RentProcessTaskApplicationTests.java
@@ -0,0 +1,18 @@
+package com.arshad.rentcloud;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class RentProcessTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
